99aa3142cee44c927e27130abac5d5080368d83a
Mehul-Bisht <mehulbishtanna@gmail.com>
2020-07-22 13:20:41 +0530

    Initial commit

---
+++ app/src/main/java/com/mehul/textrecognizer/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.mehul.textrecognizer
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        //lottie.playAnimation()
+
+        Handler().postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            lottie.cancelAnimation()
+            finish()
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+        }, 3500 )
+
+    }
+}
